4f078e875a4f4e027c3224dcb7c2582676ada06f
akcaau <gllazrv@gmail.com>
2023-02-24 21:00:06 +0300

    Добавлены исправления

--- menu/tree/urls.py
+++ menu/tree/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from .views import draw_menu, get_menu
+from .views import draw_menu
 
 app_name = "tree"
 
 urlpatterns = [
-    # path("<slug:slug>/", get_menu, name="menu"),  # адрес cтраницы меню
     path("<slug:slug>/", draw_menu, name="debug"),  # адрес cтраницы меню
 ]
-
--- menu/tree/views.py
+++ menu/tree/views.py
@@ -1,33 +1,6 @@
-from django.shortcuts import get_object_or_404, get_list_or_404, render
+from django.shortcuts import render
 
-from .models import Menu, Section
-
-
-def get_menu(request, slug):
-
-    def get_all_level(section):
-        return section.top_section.down_sections.exclude(slug=slug)
-
-    section = get_object_or_404(Section, slug=slug)
-    current_section = section  # запоминаем запрошенную секцию меню
-    # запоминаем следующий уровень
-    down_sections = current_section.down_sections.all()
-    # пустой лист для упаковки секций
-    tree = []
-    # получаем верхнюю часть дерева если она есть
-    # начинаем обход с уровня выше запрашиваемого
-    while section.top_section:
-        tree.append(get_all_level(section))
-        section = section.top_section
-    # получаем верхний уровень
-    tree.append(get_list_or_404(Section, top_section=None))
-    template = "tree/menu.html"
-    context = {
-        'current_section': current_section,
-        'tree': tree,
-        'down_sections': down_sections,
-    }
-    return render(request, template, context)
+from .models import Menu
 
 
 def draw_menu(request, slug):
@@ -49,7 +22,7 @@ def draw_menu(request, slug):
         section = section.top_section
     print(levels)
     print(active_top_sections)
-    template = "tree/debug.html"
+    template = "tree/menu.html"
     context = {
         'menu': menu,
         'sections': sections,
